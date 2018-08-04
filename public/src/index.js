9875e0e5529f125b0c113bd3516077942c94852e
Harry Dry <harrydry@Crowdforms-MacBook-Pro.local>
2018-08-04 18:35:57 +0100

    s

--- public/src/index.js
+++ public/src/index.js
@@ -1,21 +1,38 @@
 import "./sass/styles.sass";
 import { $, $$ } from "./modules/bling";
-import axios from "axios";
 
-import ajaxVote from "./modules/ajaxVote";
-import ajaxCommentVote from "./modules/ajaxCommentVote";
-import "./modules/modal";
+const mobilePlay = $(".interview__icon");
+const audio = $(".audio");
+const timer = $(".interview__text");
 
-const upVoteForms = $$(".votingButton");
-upVoteForms.on("click", ajaxVote);
+mobilePlay.on("click", () => {
+  if (!audio.paused) {
+    audio.pause();
+  } else {
+    audio.play();
+    setInterval(() => {
+      const time = Math.floor(audio.currentTime);
+      const minutes = Math.floor(time / 60);
+      const seconds = time % 60;
+      if (seconds < 10) {
+        var insertTime = ` ${minutes}:0${seconds}`;
+      } else {
+        var insertTime = ` ${minutes}:${seconds}`;
+      }
+      timer.innerHTML = insertTime;
+    }, 500);
+  }
+});
 
-const upVoteCommentForms = $$("form.commentButton");
-upVoteCommentForms.on("submit", ajaxCommentVote);
+const bamn1 = $(".bamn1");
+const bamn2 = $(".bamn2");
+const bamn3 = $(".bamn3");
 
-if (document.querySelector(".p1")) {
-  $$(".ideaList__inner").on("click", function(e) {
-    const url = `https://ideasareworthless.io${this.dataset.location}`;
-    window.location.href = url;
-    return false;
-  });
+if (window.innerWidth < 730) {
+  bamn1.src =
+    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob1.jpg";
+  bamn2.src =
+    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob3.jpg";
+  bamn3.src =
+    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob2.jpg";
 }
