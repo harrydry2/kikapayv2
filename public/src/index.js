8f91a0851472d7d180f0d52846aafbd58070e3a6
Harry Dry <harrydry@Crowdforms-MacBook-Pro.local>
2018-08-05 21:55:51 +0100

    s

--- public/src/index.js
+++ public/src/index.js
@@ -27,14 +27,17 @@ mobilePlay.on("click", () => {
 const bamn1 = $(".bamn1");
 const bamn2 = $(".bamn2");
 const bamn3 = $(".bamn3");
+const bamn4 = $(".bamn4");
 
 if (window.innerWidth < 730) {
   bamn1.src =
-    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob1.jpg";
-  bamn2.src =
-    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob3.jpg";
-  bamn3.src =
     "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob2.jpg";
+  bamn2.src =
+    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob1.jpg";
+  bamn3.src =
+    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamnmob3.jpg";
+  bamn4.src =
+    "https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/closerMob.png";
 }
 
 const bIMG = $(".front__img");
@@ -44,10 +47,39 @@ const textArray = [
   "Holborn — London",
   "Casper — Wyoming"
 ];
+const bgpArray = ["44%", "70%", "87%"];
 var frontNum = 1;
 
 setInterval(() => {
   bIMG.style.backgroundImage = `url(https://s3.eu-west-2.amazonaws.com/yeezydating/bamn/bamn${frontNum}.jpg)`;
+  if (window.innerWidth < 701) {
+    bIMG.style.backgroundPosition = bgpArray[`${frontNum - 1}`];
+  }
   bText.innerText = textArray[`${frontNum - 1}`];
   frontNum === 3 ? (frontNum = 1) : frontNum++;
 }, 3500);
+
+// fo
+
+const frontOuter = $(".frontOuter");
+const front = $(".front");
+const container = $(".container");
+let innerHeight = window.innerHeight;
+frontOuter.style.top = `-${innerHeight}px`;
+
+window.on("load", () => {
+  setTimeout(() => {
+    frontOuter.style.transform = `translateY(${innerHeight}px)`;
+    container.style.transform = `translateY(${innerHeight}px)`;
+  }, 300);
+  setTimeout(() => {
+    front.style.opacity = "1";
+  }, 1000);
+});
+
+window.on("resize", () => {
+  innerHeight = window.innerHeight;
+  frontOuter.style.top = `-${innerHeight}px`;
+  frontOuter.style.transform = `translateY(${innerHeight}px)`;
+  container.style.transform = `translateY(${innerHeight}px)`;
+});
