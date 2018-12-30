2d6b9f978b7a15980465aea4e5f6f59f88f020f2
Harry Dry <harrydry@Crowdforms-MacBook-Pro.local>
2018-12-30 16:47:30 +0000

    s

--- public/src/index.js
+++ public/src/index.js
@@ -1,55 +1,61 @@
 import "./sass/styles.sass";
 import { $, $$ } from "./modules/bling";
+import aboutFunction from "./modules/about";
+import mobileMenu from "./modules/mobileMenu";
+import Animate from "animate.js";
+import "./modules/header";
+header;
+import {
+  faqFunction,
+  radioFunction,
+  wideFunction,
+  formSubmit,
+  zenscrollFunction
+} from "./modules/merchant";
 
-const bIMG = $(".front__img");
-const bText = $(".front__label");
-const bgpArray = ["30%", "30%", "50%"];
-var frontNum = 1;
-
-setInterval(() => {
-  bIMG.style.backgroundImage = `url(https://s3.amazonaws.com/harrydry/tysonfury/fury${frontNum}.jpg)`;
-  if (window.innerWidth < 701) {
-    bIMG.style.backgroundPosition = bgpArray[`${frontNum - 1}`];
-  }
-  frontNum === 3 ? (frontNum = 1) : frontNum++;
-}, 3500);
-
-// fo
-
-const frontOuter = $(".frontOuter");
-const front = $(".front");
-const fixedSection = $(".fixedSection");
-console.log(fixedSection);
-const container = $(".container");
-let innerHeight = window.innerHeight;
-frontOuter.style.top = `-${innerHeight}px`;
-
-window.on("load", () => {
-  setTimeout(() => {
-    frontOuter.style.transform = `translateY(${innerHeight}px)`;
-    container.style.transform = `translateY(${innerHeight}px)`;
-    fixedSection.style.transform = `translateY(${innerHeight}px)`;
-  }, 300);
-  setTimeout(() => {
-    front.style.opacity = "1";
-  }, 1000);
-});
-
-window.on("resize", () => {
-  innerHeight = window.innerHeight;
-  frontOuter.style.top = `-${innerHeight}px`;
-  frontOuter.style.transform = `translateY(${innerHeight}px)`;
-  container.style.transform = `translateY(${innerHeight}px)`;
-  fixedSection.style.transform = `translateY(${innerHeight}px)`;
-});
-
-window.on("scroll", () => {
-  if (window.scrollY > innerHeight) {
-    fixedSection.style.position = "fixed";
-    fixedSection.style.transform = "translateY(0)";
-    fixedSection.style.transition = "all 0s";
-  } else {
-    fixedSection.style.position = "relative";
-    fixedSection.style.transform = `translateY(${innerHeight}px)`;
+var animate = new Animate({
+  target: "[data-animate]",
+  animatedClass: "visible",
+  offset: [0.73],
+  delay: 0,
+  remove: false,
+  reverse: true,
+  scrolled: false,
+  debug: true,
+  onLoad: true,
+  onScroll: true,
+  onResize: false,
+  callbackOnInit: function() {
+    console.log("Initialised");
+  },
+  callbackOnInView: function(element) {
+    var animationType = element
+      .getAttribute("data-animation-classes")
+      .replace("animated", "")
+      .trim();
+    console.log(animationType + " in view.");
+  },
+  callbackOnAnimate: function(element) {
+    console.log(element);
   }
 });
+
+animate.init();
+animate.render();
+
+if ($(".merchants")) {
+  faqFunction();
+  wideFunction();
+  radioFunction();
+  formSubmit();
+  zenscrollFunction();
+}
+
+if ($(".aboutSVG")) {
+  aboutFunction();
+}
+
+if (window.innerWidth <= 1023) {
+  console.log(window.innerWidth);
+  mobileMenu();
+}
---
+++ public/src/modules/header.js
@@ -0,0 +1,14 @@
+// import { $, $$ } from "./bling";
+
+// const header = $(".header");
+
+// window.on("scroll", headerScroll);
+
+// function headerScroll() {
+//   if (window.scrollY > 20) {
+//     header.classList.add("headerScroll");
+//   }
+//   if (window.scrollY < 20 && header.classList.contains("headerScroll")) {
+//     header.classList.remove("headerScroll");
+//   }
+// }
