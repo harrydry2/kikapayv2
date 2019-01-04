544161bad60bc0ecf540dca9ed77b078cf4e5688
Harry Dry <harrydry@Crowdforms-MacBook-Pro.local>
2019-01-04 17:40:30 +0000

    s

--- public/src/modules/header.js
+++ public/src/modules/header.js
@@ -1,14 +1,14 @@
-// import { $, $$ } from "./bling";
+import { $, $$ } from "./bling";
 
-// const header = $(".header");
+const header = $(".header");
 
-// window.on("scroll", headerScroll);
+window.on("scroll", headerScroll);
 
-// function headerScroll() {
-//   if (window.scrollY > 20) {
-//     header.classList.add("headerScroll");
-//   }
-//   if (window.scrollY < 20 && header.classList.contains("headerScroll")) {
-//     header.classList.remove("headerScroll");
-//   }
-// }
+function headerScroll() {
+  if (window.scrollY > 20) {
+    header.classList.add("headerScroll");
+  }
+  if (window.scrollY < 20 && header.classList.contains("headerScroll")) {
+    header.classList.remove("headerScroll");
+  }
+}
